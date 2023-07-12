0fad9e22b6260b4200cb826caadbb3b7f1aa2b97
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-07-12 08:44:45 -0700

    Cors Error

--- mySpotifyModule/src/App/App.js
+++ mySpotifyModule/src/App/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, InputGroup, FormControl, Button, Card, Row } from "react-bootstrap";
@@ -8,77 +9,108 @@ const CLIENT_SECRET = '256582d4f9d04a7f82631a7ec7cf5945'
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
-  const [albums, setAlbums] = useState([]); //Taylor Swift
+  const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
-    // API Access Token
     var authParameters = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https:///accounts.spotify.com/api/token', authParameters)
-      .then(results => results.json())
-      .then(data => setAccessToken(data.access_token))
-  }, [])
+    };
 
-  // Search
+    fetch('https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token', authParameters)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        setAccessToken(data.access_token);
+      })
+      .catch(error => {
+        console.error('Error:', error.message);
+      });
+  }, []);
+
   async function search() {
-    console.log("Search for " + searchInput); //Taylor Swift
+    console.log("Search for " + searchInput);
 
     var searchParameters = {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + accessToken
+        'Authorization': 'Bearer ' + accessToken,
       }
-    }
-    var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
-      .then(response => response.json())
-      .then(data => data.artists.items[0].id)
+    };
 
-      console.log("Artist ID is " + artistID);
-    // Get request with Artist ID grab all the albums from that artist
-    var returnedAlbums = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums' + '?include_groups=album&market=US&limit=50', artistParameters)
-    .then(response => response.json())
-    .then(data => { return data.items })
-      console.log(data)
-      setAlbums(data.items);
-    //Display all the albums
+    try {
+      var artistID = await fetch('https://api.spotify.com/v1/search?q=' + searchInput + '&type=artist', searchParameters)
+      if (!artistIDResponse.ok) {
+        throw new Error('Network response was not ok');
+      }
+      var data = await artistIDResponse.json();
+      var artistID = data.artists.items[0].id
+
+      console.log(artistID);
+
+      var returnedAlbumsResponse = await fetch('https://api.spotify.com/v1/artists/' + artistID + '/albums?include_groups=album,single&limit=50&market=US', searchParameters);
+      if (!returnedAlbumsResponse.ok) {
+        throw new Error('Network response was not ok');
+      }
+      var returnedAlbumsData = await returnedAlbumsResponse.json();
+      var returnedAlbums = returnedAlbumsData.items;
+      
+
+      console.log(returnedAlbums);
+      setAlbums(returnedAlbums);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
   }
+
   console.log(albums);
+
   return (
     <div className="App">
       <Container>
         <InputGroup className="mb-3" size="lg">
           <FormControl
+            autoComplete="on"
+            autoCorrect="on"
             placeholder="Search for an artist"
             type="input"
-            onKeyDown={event => {
+            onKeyPress={event => {
               if (event.key === "Enter") {
                 search();
               }
             }}
             onChange={event => setSearchInput(event.target.value)}
           />
-          <Button onClick={() => { console.log("Clicked Button") }}>
+          <Button className="btn btn-success" onClick={search}>
             Search
           </Button>
         </InputGroup>
-        </Container>
-        <Container>
-          <Row className="mx-2 row row-cols-4">
-          <Card>
-            <Card.Img src='#' />
-          <Card.Body>
-            <Card.Title>Album Name Here</Card.Title>
-          </Card.Body>
-          </Card>
-          </Row>
-        </Container>
-        </div>
-        )
-  }
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-cols-4">
+          {albums.map((album, i) => {
+            console.log(album);
+            return (
+              <Card key={i}>
+                <Card.Img src={album.images[0].url} />
+                <Card.Body>
+                  <Card.Title>{album.name}</Card.Title>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
 export default App;
